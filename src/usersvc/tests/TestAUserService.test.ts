992aa4b756623e6f5becbd87c2ada37631a5266f
Dean <dean@Deans-MacBook-Pro.local>
2022-10-17 10:54:08 +0530

    almost there

--- src/usersvc/tests/TestAUserService.test.ts
+++ src/usersvc/tests/TestAUserService.test.ts
@@ -1,7 +1,7 @@
 import UserService from "../api/UserService";
 import UserServiceFactory from "../api/UserServiceFactory";
 
-let userSvc: UserService;
+let userSvc: UserService = null;
 
 const id1 = 4;
 const id2 = 5;
